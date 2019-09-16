0f030eb21e55c351a27978de6559c7e4f5eba47f
Isabella Soares <isabella.lima@goomer.com.br>
2019-09-16 14:25:01 -0300

    Add Object.entries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Data from './data.json';
+import DataTest from './data_test.json';
 import './App.css';
 import RecursiveList from './components/recursive_list/recursive.js';
 
-
 class App extends Component {
   render () {
-    return (
+    {console.log(Object.entries(DataTest));}
+    return (      
       <RecursiveList data={Data} />
     );
   }
