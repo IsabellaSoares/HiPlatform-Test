c2fbf5f72f55109a0926e14bd9ed2f9050ff8846
Isabella <isabella.soares.28@gmail.com>
2019-09-16 22:16:46 -0300

    Final project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Tree } from 'antd';
 import Data from './data.json';
-import DataTest from './data_test.json';
-import './App.css';
-import RecursiveList from './components/recursive_list/recursive.js';
+
+const { TreeNode } = Tree;
+const treeData = Object.entries(Data);
 
 class App extends Component {
-  render () {
-    {console.log(Object.entries(DataTest));}
-    return (      
-      <RecursiveList data={Data} />
+  state = {
+    expandedKeys: JSON.parse(localStorage.getItem("expandedKeys")),
+    checkedKeys: JSON.parse(localStorage.getItem("checkedKeys")),
+    selectedKeys: JSON.parse(localStorage.getItem("selectedKeys")),
+  };
+
+  onExpand = expandedKeys => {
+    this.setState({ expandedKeys });
+    localStorage.setItem("expandedKeys", JSON.stringify(expandedKeys));
+  };
+
+  onCheck = checkedKeys => {
+    this.setState({ checkedKeys });
+    localStorage.setItem("checkedKeys", JSON.stringify(checkedKeys));
+  };
+
+  onSelect = (selectedKeys) => {
+    this.setState({ selectedKeys });
+    localStorage.setItem("selectedKeys", JSON.stringify(selectedKeys));
+  };
+
+  renderTreeNodes = data =>    
+    data.map(item => {
+      let node = item[1];
+      let node_children = Object.entries(item[1].children);
+
+      return (
+        <TreeNode title={node.name} key={node.id} dataRef={node}>
+          {node_children.length > 0 && this.renderTreeNodes(node_children)}
+        </TreeNode>
+      );
+    });
+
+  render() {
+    return (
+      <Tree
+        checkable
+        onExpand={this.onExpand}
+        expandedKeys={this.state.expandedKeys}
+        onCheck={this.onCheck}
+        checkedKeys={this.state.checkedKeys}
+        onSelect={this.onSelect}
+        selectedKeys={this.state.selectedKeys}
+      >
+        {this.renderTreeNodes(treeData)}
+      </Tree>
     );
   }
-}
+}          
 
 export default App;
